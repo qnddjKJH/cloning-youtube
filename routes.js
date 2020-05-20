eba47ceda12602d2892d3a40860e9d9360d05e23
qnddjKJH <qnddjKJH.dev@gmail.com>
2020-05-20 19:31:26 +0900

    Added and Modified Files
    express 를 다루는 방법 및 연습
    라우터에 대한 공부 및 연습 그리고 팁
    	- routes 로 하나로 정리 하기.
    npm 과 여러 package 그리고 middleware 에 대한 여러 공부.
    	- express ( 서버 )
    	- morgan ( logger )
    	- helmet ( 보안 )
    	- cookie-parser ( 쿠키에 대한 간편 작업 )
    	- body-parser ( 데이터에 대한 간편 작업 )
    
    ExpressJS 는 프레임워크

---
+++ routes.js
@@ -0,0 +1,37 @@
+// global
+const HOME = "/";
+const JOIN = "/join";
+const LOGIN = "/login";
+const LOGOUT = "/logout";
+const SEARCH = "/search";
+
+// Users
+const USERS = "/users";
+const USER_DETAIL = "/:id";
+const EDIT_PROFILE = "/edit-profile";
+const CHANGE_PASSWORD = "/change-password";
+
+// Videos
+const VIDEOS = "/videos";
+const VIDEO_DETAIL = "/:id";
+const EDIT_VIDEO = "/:id/edit";
+const DELETE_VIDEO = "/:id/delete";
+
+// Object
+const routes = {
+    home: HOME,
+    join: JOIN,
+    login: LOGIN,
+    logout: LOGOUT,
+    search: SEARCH,
+    users: USERS,
+    userDetail: USER_DETAIL,
+    editProfile: EDIT_PROFILE,
+    changePassword: CHANGE_PASSWORD,
+    videos: VIDEOS,
+    videoDetail: VIDEO_DETAIL,
+    editVideo: EDIT_VIDEO,
+    deleteVideo: DELETE_VIDEO
+};
+
+export default routes;
